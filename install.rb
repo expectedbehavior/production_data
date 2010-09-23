c245c1eb9385adf5250910de4514cd7ff645631e
Jason Gladish <jason@expectedbehavior.com>
2010-09-22 23:21:56 -0400

    playing with install

--- install.rb
+++ install.rb
@@ -1,8 +1,10 @@
 require 'fileutils'
 require File.join(File.dirname(__FILE__), "lib", "production_data_helpers.rb")
 
+rails_root = File.join(File.dirname(__FILE__), '..', '..', '..')
+
 relative_db_setup_path = File.join("script", "db_setup")
-dest_db_setup = File.join(RAILS_ROOT, relative_db_setup_path)
+dest_db_setup = File.join(rails_root, relative_db_setup_path)
 if File.exist?(dest_db_setup)
   puts
   puts "db_setup installed to #{relative_db_setup_path}, check it out and edit it appropriately"
@@ -10,7 +12,7 @@ else
   FileUtils.cp File.join(File.dirname(__FILE__), relative_db_setup_path), dest_db_setup
 end
 
-dest_production_data_config = File.join(RAILS_ROOT, ProductionDataHelpers::RELATIVE_CONFIG_PATH)
+dest_production_data_config = File.join(rails_root, ProductionDataHelpers::RELATIVE_CONFIG_PATH)
 if File.exist?(dest_production_data_config)
   puts
   puts "Example config installed to #{ProductionDataHelpers::RELATIVE_CONFIG_PATH}, check it out and edit it appropriately"
